8c4eb1cb81ce39fe7988219c3eaf0f8ae6a1c009
Victor Belikov <v1ctor.belikov@gmail.com>
2019-08-29 07:29:41 +0300

    1_ITDoctor

--- 1_ITDoctor/gulpfile.js
+++ 1_ITDoctor/gulpfile.js
@@ -1,1 +1,18 @@
 // https://www.youtube.com/watch?v=jjqvl0M0tGw
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const watch = require('gulp-watch');
+
+gulp.task('sass-compile', () => {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./')) // текущая берется от .dest()
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch', () => {
+  // за какими файлами следим; какой таск запускаем при измении файлов
+  gulp.watch('./scss/**/*.scss', gulp.series('sass-compile'));
+});
